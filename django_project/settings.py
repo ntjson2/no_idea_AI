86afb00af7cd555028eecae709075a62b8e2a40a
ntjson2 <ntjson@gmail.com>
2022-10-27 21:54:37 -0600

    configs

--- django_project/settings.py
+++ django_project/settings.py
@@ -137,6 +137,6 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 DEFAULT_FROM_EMAIL = "sporkrunner@gmail.com"
 EMAIL_HOST = "smtp.sendgrid.net"
 EMAIL_HOST_USER = "apikey"
-EMAIL_HOST_PASSWORD = "found in sendgrid_pw file"
+EMAIL_HOST_PASSWORD = env.str("EMAIL_HOST_PASSWORD")
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
