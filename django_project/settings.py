7f7b10892f9a711d2b046d2dd198762d458adce5
ntjson2 <ntjson@gmail.com>
2022-10-27 21:23:04 -0600

    env file

--- django_project/settings.py
+++ django_project/settings.py
@@ -13,7 +13,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-f8+707y=j$ytss(tfmk+)%5#@t98^63t&tx^p4culc3%plnq($"
+SECRET_KEY = env.str("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DEBUG", default=False)
