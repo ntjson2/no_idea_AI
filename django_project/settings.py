c4b04b1f0633eb4ed8bc201935180431d660045c
ntjson2 <ntjson@gmail.com>
2022-10-27 21:27:57 -0600

    config database

--- django_project/settings.py
+++ django_project/settings.py
@@ -74,10 +74,7 @@ WSGI_APPLICATION = "django_project.wsgi.application"
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": env.dj_db_url("DATABASE_URL")
 }
 
 
