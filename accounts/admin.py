9419e0bca12aec893a5ad1a7cc0d1f8be11febf5
ntjson2 <ntjson@gmail.com>
2022-10-27 21:13:00 -0600

    initial commit

---
+++ accounts/admin.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+from .models import CustomUser
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserChangeForm
+    form = CustomUserChangeForm
+    model = CustomUser
+    list_display = [
+        'email',
+        'username',
+        'age',
+        'is_staff',
+    ]
+    fieldsets = UserAdmin.fieldsets + ((None, {'fields': ('age',)}),)
+    add_fieldsets = UserAdmin.fieldsets + ((None, {'fields': ('age',)}),)
+
+admin.site.register(CustomUser, CustomUserAdmin)
